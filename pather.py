38ce07dc4c74fb27c2336628810f2cb1c9d4e13b
Anshal Jain <pk.ness.ness@gmail.com>
2023-05-12 16:35:09 -0700

    errors with auto-compile

--- pather.py
+++ pather.py
@@ -1,33 +1,33 @@
-import boto3, cv2, pygame, globals, pygame_widgets, sys
-from pygame_widgets.slider import Slider
-from multiprocessing import Process
-try:
-    from PyQt5.QtCore import Qt
-except ModuleNotFoundError: 
-    pip.main(['install', 'PyQt5'])
-    from PyQt5.QtCore import Qt
+# import boto3, cv2, pygame, globals, pygame_widgets, sys
+# from pygame_widgets.slider import Slider
+# from multiprocessing import Process
+# try:
+#     from PyQt5.QtCore import Qt
+# except ModuleNotFoundError: 
+#     pip.main(['install', 'PyQt5'])
+#     from PyQt5.QtCore import Qt
 
-from PyQt5.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QComboBox,
-    QDial,
-    QDoubleSpinBox,
-    QLabel,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QVBoxLayout,
-    QWidget,
-)
-import PyQt5
+# from PyQt5.QtWidgets import (
+#     QApplication,
+#     QCheckBox,
+#     QComboBox,
+#     QDial,
+#     QDoubleSpinBox,
+#     QLabel,
+#     QMainWindow,
+#     QProgressBar,
+#     QPushButton,
+#     QRadioButton,
+#     QSlider,
+#     QSpinBox,
+#     QVBoxLayout,
+#     QWidget,
+# )
+# import PyQt5
 
-print('Path to module:',cv2.__file__)
-print('Path to module:',boto3.__file__)
-print('Path to module:',pygame.__file__)
-print('Path to module:',pygame_widgets.__file__)
-#print('Path to module:',sys.__file__)
-print('Path to module:',PyQt5.__file__)
+# print('Path to module:',cv2.__file__)
+# print('Path to module:',boto3.__file__)
+# print('Path to module:',pygame.__file__)
+# print('Path to module:',pygame_widgets.__file__)
+# #print('Path to module:',sys.__file__)
+# print('Path to module:',PyQt5.__file__)
